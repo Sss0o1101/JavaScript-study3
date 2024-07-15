6698903169af89cc738bb993a6c65eebcc6f9e8c
sss <>
2024-07-15 17:18:56 +0900

    test

---
+++ script1.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log(100);
+console.log(-10);
+console.log(2.5);
+
+// 12000.12000
+console.log(1.2e4); //1.2×10の4乗 = 12000
+console.log(1.2e-4); //1.2×10の-4乗 = 0.00012
+
+/*
+JavaScript
+コメント
+*/
+
+test 
