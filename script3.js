b37668fed4e7fda1ba23b373874ba493e46ea754
sss <>
2024-07-15 16:47:33 +0900

    More JavaScript text added

--- script3.js
+++ script3.js
@@ -14,3 +14,18 @@ price = 151;
 console.log(price * 120 * rate);
 console.log(price * 130 * rate);
 console.log(price * 140 * rate);
+
+//変数に値を再代入するときによく使われる演算子
+price = price + 1;
+price += 1;
+price++;
+
+
+//-----------------------------------------------
+
+
+//・文字列
+//JavaScriptで文字列を表現するための方法について見ていきます。
+console.log("it is a pen");
+console.log("it's a pen");
+
