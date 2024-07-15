4ad5563ca0eb49cce2e476f527a244e52a779c5b
sss <>
2024-07-15 16:16:24 +0900

    added JavaScript text

---
+++ script3.js
@@ -0,0 +1,16 @@
+//変数について見ていきます。
+
+// 変数
+let price;
+// 定数
+const rate = 1.1;
+
+price = 150;
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
+
+price = 151;
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
