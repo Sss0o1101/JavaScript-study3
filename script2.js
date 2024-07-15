edf0f1241e2c8da86af1f4e62a4efcc6273a7c4f
sss <>
2024-07-15 16:16:01 +0900

    More JavaScript text added

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//演算子
+//・演算子
 //さまざまな数値計算をするための演算子について見ていきます。
 console.log(10 + 3); // 13
 console.log(10 - 3); // 7
@@ -13,5 +13,12 @@ console.log(10 + 2 * 3); // 16
 console.log((10 + 2) * 3); // 36
 
 
-//定数
+//・定数
 // 数値に名前をつけて管理することができる定数について見ていきます。
+const price = 150;
+const rate = 1.1;
+
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
+
