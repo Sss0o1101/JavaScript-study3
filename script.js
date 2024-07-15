c43032f29e4128889ee5f2ccd06ef4c3afb6183b
sss <>
2024-07-15 15:43:47 +0900

    More JavaScript text added

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 console.log(100);
 console.log(-10);
 console.log(2.5);
+
+// 12000.12000
+console.log(1.2e4); //1.2×10の4乗 = 12000
+console.log(1.2e-4); //1.2×10の-4乗 = 0.00012
+
+/*
+JavaScript
+コメント
+*/
+
